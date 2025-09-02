c549f27ecdf0e85ff9cdc2f60e9bf5d1ea394f3a
zabronax <zabronax@gmail.com>
2025-09-02 09:48:34 +0200

    outline first test

---
+++ Tests/Authentication.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests;
+
+public class Authentication
+{
+    [Fact]
+    public void Register_Endpoint_Should_Allow_Registering_New_ID()
+    {
+        // Arrange
+        // Noe å sende meldinger med
+        // Serveren å sende meldinger til
+        // Vår hemmelige ID
+        // Det "ID kort" (JWT) vi forventer å få tilbake 
+
+        // Act
+        // Når vi sender en melding til endepunktet
+        // Så får vi tilbake et result
+
+        // Assert
+        // Resultat skal stemme overens med våre antakelser
+    }
+}
